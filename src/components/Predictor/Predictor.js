76054f1616651a4b61ce8494adfffcec1bd992e4
Ayesha Bhatti <abhatt13@mail.bbk.ac.uk>
2022-09-09 15:04:27 +0100

    Changing predictor code

--- src/components/Predictor/Predictor.js
+++ src/components/Predictor/Predictor.js
@@ -4,15 +4,10 @@ import './Predictor.css'
 const Predictor = (props) => {
 
   const [data, setData] = useState('')
-  const [prediction, setPrediction] = useState('')
 
   useEffect( () => {
     setData(props.value)
   }, [props.value])
-
-  const isEmpty = (p) => {
-    return p === ''
-  }
 
   const handleClick = async () => {
 
@@ -32,62 +27,28 @@ const Predictor = (props) => {
       }
     })
 
-    const prediction_json = await prediction_response.json()
-    const prediction_arr = Object.entries(prediction_json)
-
     if (prediction_response.ok) {
-      setPrediction(prediction_arr)
+      const prediction_json = await prediction_response.json()
+      const prediction_arr = Object.entries(prediction_json)
+
+      const response = await fetch('https://denovo-cms.herokuapp.com/api/cases/' + data._id, {
+        method: 'PATCH',
+        body: JSON.stringify({'partyWinning': prediction_arr}),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const json = await response.json()
+  
+      if (response.ok) {
+        console.log("Matter updated", json)
     }
-
-    const response = await fetch('https://denovo-cms.herokuapp.com/api/cases/' + data._id, {
-      method: 'PATCH',
-      body: JSON.stringify({'partyWinning': prediction_arr}),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    const json = await response.json()
-
-    if (response.ok) {
-      console.log("Matter updated", json)
   }
 }
 
     return (
         <div className='predict'>
             <button className= 'predict-button' onClick={ handleClick }>Make prediction</button>
-            <div className='predict-output'>
-              { isEmpty(data) ? 'Loading...' : 
-                !isEmpty(data.partyWinning[0]) ? (
-                  <ul>
-                  <li>
-                    {'Likelihood of success: ' + data.partyWinning[1][1] }
-                  </li>
-                  <li>
-                    {'Likelihood of failure: ' + data.partyWinning[0][1]}
-                  </li>
-                  <li>
-                    {'Likelihood of an unclear result: ' + data.partyWinning[2][1]}
-                  </li>
-                </ul>
-              ) :
-              isEmpty(prediction) ?
-                  (
-                    'Loading...'
-                  ) : (
-                    <ul>
-                      <li className='text'>
-                        {'Likelihood of success: ' + prediction[1][1] }
-                      </li>
-                      <li className='text'>
-                        {'Likelihood of failure: ' + prediction[0][1]}
-                      </li>
-                      <li className='text'>
-                        {'Likelihood of an unclear result: ' + prediction[2][1]}
-                      </li>
-                    </ul>
-                )}
-            </div>
         </div>
     )
 }
